5f915085371d53c4b81eeae9903de2ce81a8f49e
Jordi <jordijs6@gmail.com>
2024-05-14 19:55:08 +0200

    Complete 2.3*: Course information step8

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -4,18 +4,10 @@ import Total from "./Total"
 
   const Course = ({course}) => {
 
-    const total = () =>{
-      let sum = 0
-      for (const part of course.parts){
-        sum += part.exercises
-      }
-      return sum
-    }
-
     return <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      <Total sum={total()} />
+      <Total parts={course.parts} />
     </div>
 
   }
--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,2 +1,9 @@
-const Total = ({ sum }) => <p><strong>Number of exercises {sum}</strong></p>
+const Total = ({ parts }) => {
+  const total = parts.reduce((accumulator, item) => (
+    accumulator + item.exercises
+  ), 0)
+
+  return <p><strong>Number of exercises {total}</strong></p>
+}
+
 export default Total
